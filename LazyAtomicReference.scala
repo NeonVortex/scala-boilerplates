371c22fd488728463bd522fb7cd1fa374939978a
Hance <texasbruce@users.noreply.github.com>
2019-07-04 00:20:40 -0400

    Create LazyAtomicReference.scala

---
+++ LazyAtomicReference.scala
@@ -0,0 +1,41 @@
+import java.util.concurrent.atomic.AtomicReference
+import scala.concurrent._
+import scala.concurrent.duration.Duration
+
+class LazyAtomicReference[T](@volatile var value: T) {
+    def compareAndSet(v: => T, u: => T) = {
+        if (value == v) {
+            synchronized {
+                if(value == v) {
+                    value = u
+                    true
+                } else false
+            }
+        }
+        else false
+    }
+
+    def get = value
+}
+
+//Test
+class Foo {
+    val s = new LazyAtomicReference[String](null)
+
+    def foo(): String = {
+        println("called")
+        "foo"
+    }
+
+    def get() = {
+        s.compareAndSet(null, foo())
+        s.get
+    }
+}
+
+val f = new Foo
+implicit val ec = scala.concurrent.ExecutionContext.global
+(1 to 100) foreach { _ =>
+    Future { assert(f.get() != null) }
+}
+Await.result(Future.never, Duration.Inf)
